d9c0ea0922f25b760a68ec1877b4d404bef00f9f
Tamara Carlos <tamaraaselau@gmail.com>
2024-01-19 19:26:57 +0000

    Update sentence

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>The "good old days" are always happening right now.</h1>
+    return <h1>The "good old days" are always happening right now!</h1>
 }
 
 export default Home;
